b1cd813ae03fe26a322e710f44dd5fc61f62e569
Nguyen Phi Vu <dinofx1999@gmail.com>
2025-12-02 13:47:08 +0700

    update

--- routes/Module/Jobs/Analysis.js
+++ routes/Module/Jobs/Analysis.js
@@ -26,8 +26,10 @@ const {Insert_UpdateAnalysisConfig} = require('../../Database/analysis-config.he
             if( CURRENT.typeaccount === "STD") SPREAD_MIN_CURRENT = symbolConfig_data.Spread_STD;
             if( CURRENT.typeaccount === "ECN") SPREAD_MIN_CURRENT = symbolConfig_data.Spread_ECN;
         }
+        
 
-        if(symbol === "BTCUSD" && CURRENT.Broker === "FXBIG")console.log(`Session: ${SESSION} |Spread_Min: ${SPREAD_MIN_CURRENT} | Spread_X: ${SPREAD_X_CURRENT} | CURRENT.typeaccount : ${CURRENT.typeaccount }`);
+        // if(symbol === "BTCUSD" && CURRENT.Broker === "FXBIG")console.log(`Session: ${SESSION} |Spread_Min: ${SPREAD_MIN_CURRENT} | Spread_X: ${SPREAD_X_CURRENT} | CURRENT.typeaccount : ${CURRENT.typeaccount }`);
+        if(symbol === "BTCUSD" && CURRENT.Broker === "FXBIG")console.log(CURRENT);
 
         //Check BUY
         let Spread_Sync = parseFloat(SPREAD_MIN_CURRENT * SPREAD_X_CURRENT);
