c5f39c07522ab5190d6814a8400862d5b2204e9c
Nguyen Phi Vu <dinofx1999@gmail.com>
2025-12-03 15:42:10 +0700

    update

--- routes/Module/Jobs/Analysis_Type2.js
+++ routes/Module/Jobs/Analysis_Type2.js
@@ -54,10 +54,7 @@ async function Analysis_Type2(data, symbol, symbolConfig_data) {
         // 3. CHỈ PHÂN TÍCH CÁC BROKER BỊ LỆCH
         // ═══════════════════════════════════════════════════════════
         for (const CURRENT of outlierGroup) {
-        //      if (Number(CURRENT.timedelay) < Number(process.env.MAX_NEGATIVE_DELAY) * 60 || (-30 * 60)) {
-        // // console.log(`[SKIP] ${symbol} | ${CURRENT.Broker} delay quá lớn: ${CURRENT.timedelay}ms`);
-        //             return;  // Bỏ qua, không phân tích
-        //         }
+            
             await analyzeSignal(CHECK, CURRENT, symbol, symbolConfig_data, digit);
         }
 
@@ -142,7 +139,7 @@ async function analyzeSignal(CHECK, CURRENT, symbol, symbolConfig_data, digit) {
     
 
     
-   
+    if(symbol === "XAUUSD" && CURRENT.Broker === "Vu Test") console.log(`Analyzing ${symbol} | ${CURRENT.Broker} | Delay: ${CURRENT.timedelay}ms`);
 
     // Lấy config spread
     let SPREAD_MIN_CURRENT = Number(CURRENT.spread_mdf);
